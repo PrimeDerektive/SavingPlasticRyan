ce0445cca97f43059fa39e615fa46b62f78cbfc1
PrimeDerektive <derek.s.traver@gmail.com>
2016-09-18 18:46:02 -0400

    initial commit

---
+++ SavingPlasticRyan/Assets/Scripts/MoveOnPath.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var timeToComplete : float = 30.0;
+var path : Transform[];
+
+function Start () {
+
+	//move along path
+	iTween.MoveTo( gameObject, iTween.Hash(
+	     "orientToPath", true,
+	     "path", path,
+	     "time", timeToComplete,
+	     "easeType", iTween.EaseType.linear
+	 ));
+}
+
